a3bd7ef8a6a1fd83a728a6685f9c1ea423b809a3
Daniel Montenegro <dannielmdr7@gmail.com>
2026-02-03 10:23:39 -0500

    feature: configuración como cliente de eureka listo

--- src/test/java/com/books/books/BooksApplicationTests.java
+++ src/test/java/com/books/books/BooksApplicationTests.java
@@ -2,12 +2,15 @@ package com.books.books;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
 class BooksApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Test que verifica que el contexto de Spring se carga correctamente
 	}
 
 }
